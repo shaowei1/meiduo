2ee9df846fc61689bbe98313aa0eae43dd5c607a
wsx <wsx36524@163.com>
2018-12-01 15:28:49 +0800

    sina_oauth

---
+++ meiduo_mall/meiduo_mall/apps/oauth/sina_oauth.py
@@ -0,0 +1,58 @@
+# from urllib.parse import urlencode
+
+import requests
+import json
+
+from future.backports.urllib.parse import urlencode
+
+
+class OAuthSina(object):
+    def __init__(self, client_id=None, client_secret=None, redirect_uri=None, state=None):
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self.redirect_uri = redirect_uri
+        self.state = state   # 用于保存登录成功后的跳转页面路径
+
+
+    def get_sina_url(self):
+        # QQ登录url参数组建
+        data_dict = {
+            'response_type': 'code',
+            'client_id': self.client_id,
+            'redirect_uri': self.redirect_uri,
+            'state': self.state
+        }
+
+        # 构建url
+        # from urllib.parse import urlencode
+        sina_url = 'https://api.weibo.com/oauth2.0/authorize?' + urlencode(data_dict)
+
+        return sina_url
+
+    def get_access_token(self, code):  # 获取用户token和uid
+        url = "https://api.weibo.com/oauth2/access_token"
+
+        querystring = {
+            "client_id": self.client_id,
+            "client_secret": self.client_key,
+            "grant_type": "authorization_code",
+            "code": code,
+            "redirect_uri": self.redirect_uri
+        }
+
+        response = requests.request("POST", url, params=querystring)
+
+        return json.loads(response.text)
+
+    def get_user_info(self, access_token_data):
+        url = "https://api.weibo.com/2/users/show.json"
+
+        querystring = {
+            "uid": access_token_data['uid'],
+            "access_token": access_token_data['access_token']
+        }
+
+        response = requests.request("GET", url, params=querystring)
+
+        return json.loads(response.text)
+
